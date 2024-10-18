97fcfcb68969b1ba0e309cd92f2a38574e2fcca1
Victor Quertelet <victor.quertelet@gmail.com>
2024-10-18 10:10:27 +0200

    Fix import name

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import {RouterProvider} from 'react-router-dom';
-import router from './Router';
+import router from './router';
 import './styles/index.scss';
 
 createRoot(document.getElementById('root')).render(
